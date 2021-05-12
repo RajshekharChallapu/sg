3d06ad1119292114c45155878e74c68edfd5d70b
Raja Challapu <raja@Rajas-Mac-mini.local>
2021-05-12 11:08:22 -0700

    spiiner reusable component

---
+++ newapp/components/classcomponentsapp/seasons/src/index.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
+
+
+class App extends React.Component {
+  state = {
+    lat: null,
+    timeStamp: null,
+    errorMessage: ''
+  }
+
+
+// use this if we are going to need location only once
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude, timeStamp: position.timestamp,}),
+      err => this.setState({errorMessage: err.message})
+    );
+  }
+
+
+render(){
+   if (this.state.errorMessage && !this.state.lat) {
+     return <div> Error: {this.state.errorMessage}</div>;
+   }
+  if (!this.state.errorMessage && this.state.lat) {
+    return <SeasonDisplay lat={this.state.lat} />;
+  }
+  return <Spinner message="Please accept location request"/>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root')
+)
+
+
+
+
